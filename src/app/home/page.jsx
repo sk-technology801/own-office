93a08508338430f9fb539aa3b76feea1947920dc
sk-technology801 <sardarsaadisaadi@gmail.com>
2025-08-13 21:33:17 +0500

    push
    hero page edit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -74,7 +74,7 @@ export default function Home() {
             <div 
               className="absolute inset-0 bg-cover bg-center"
               style={{
-                backgroundImage: `url('https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80')`,
+                backgroundImage: `url('/images/hero png.png')`,
               }}
             />
             
